98c03219119477924f8bd4338be8abb08230fb23
Lachezar Yankov <lachezar.yankov@lendo.se>
2012-12-18 23:55:49 +0200

    fix for the unittest after the refactoring

--- test_cows_and_bulls.py
+++ test_cows_and_bulls.py
@@ -1,14 +1,15 @@
 import unittest
 import cows_and_bulls
+from ai import AI
 
 class TestCowsAndBulls(unittest.TestCase):
 
     def setUp(self):
-        self.cnb = cows_and_bulls.Game()
+        self.ai = AI()
 
     def test_contrain(self):
-        self.cnb.possible_numbers = ((1,2,3,4), (1,2,3,5), (1,2,3,6), (7,8,9,0), (1,2,5,6))
-        result = self.cnb.constrain((1,2,5,6), {'cows': 1, 'bulls': 2})
+        self.ai.possible_numbers = ((1,2,3,4), (1,2,3,5), (1,2,3,6), (7,8,9,0), (1,2,5,6))
+        result = self.ai.constrain((1,2,5,6), {'cows': 1, 'bulls': 2})
         self.assertEquals(result, ((1,2,3,5),))
 
 if __name__ == '__main__':
